232cdb154b2d59ed39b542f9722f376c87715949
Ricardo Flores <ricardo.flores.tyc@gmail.com>
2023-06-16 21:41:45 -0600

    subir commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { GlobalProvider } from './context/GlobalState';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import TransactionForm from './components/transactions/TransactionForm';
+import TransactionList from './components/transactions/TransactionList';
+
+
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <Balance />
+      <TransactionForm />
+      <TransactionList />
+      <h1>Hello World</h1>
+    </GlobalProvider>
+  )
+}
+
+export default App
---
+++ src/components/Balance.jsx
@@ -0,0 +1,14 @@
+import { useGlobalState } from '../context/GlobalState'
+
+function Balance() {
+  const data = useGlobalState();
+  
+  return (
+    
+    <div>
+      <h1>Balance</h1>
+    </div>
+  )
+}
+
+export default Balance
